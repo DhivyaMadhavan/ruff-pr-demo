9c9b4217ada9a7bdcf023cb426da8bd82d34a5f5
dhivyamadhv <dhivyamadhavan@hclguvi.in>
2026-06-09 18:06:58 +0530

    Test Ruff Reviewdog workflow

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import os
-
+import math
 name="Disha"
 
 print(name)
+
+print = "This breaks the print function"
+user_greeting = f"Hello {username}"
+
+return user_greeting
+print("This will never run")
+
+
